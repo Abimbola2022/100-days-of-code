e6fa402dec43adebc528acd15646f6d6a66b33cc
Abimbola2022 <olaideabimbola2021@gmail.com>
2022-06-26 20:35:37 -0400

    100 days of code

---
+++ main.py
@@ -0,0 +1,15 @@
+i = 1
+
+while i <= 10:
+    if i == 4:
+        print("game over")
+    elif i == 8:
+        print("you're lucky")
+    else:
+        print(i)
+    i += 1
+
+for i in range(10):
+    print(i)
+    i = 5
+
