8b8293b9ad12d7fd17199fc5e985d22921872737
Abimbola2022 <olaideabimbola2021@gmail.com>
2022-06-29 09:35:27 -0400

    100-days-of-code

--- day2.py
+++ day2.py
@@ -3,5 +3,5 @@ total_bill = input("What was the total bill? $")
 split_bill = input("How many people to split the bill ")
 percentage_of_tip = input("What percentage tip would you like to give? 10, 12, or 15 ")
 
-print("Each person should pay: ")
+
 print("Each person should pay: ")
