f63003fff5d22a0caebaa72178c2a7639d6c2568
Abimbola2022 <olaideabimbola2021@gmail.com>
2022-06-29 08:09:27 -0400

    100 days of code

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-a = input("a: ")
-b = input("b: ")
-
-c = a
-a = b
-b = c
-
-print("a: " + a)
-print("b: " + b)
+print("Welcome to the band name generator")
+name_of_city = input("what's the name of city you grew up in ?\n")
+name_of_pet = input('what is the name of your pet ?\n')
+print("Your band name could be " + name_of_city + " " + name_of_pet + "\n")
