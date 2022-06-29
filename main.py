1e08e01f74961433fe12ac1162d4d6ea2362ecf0
Abimbola2022 <olaideabimbola2021@gmail.com>
2022-06-29 05:52:06 -0400

    100 days of code

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-i = 1
-
-while i <= 10:
-    if i == 4:
-        print("game over")
-    elif i == 8:
-        print("you're lucky")
-    else:
-        print(i)
-    i += 1
-
-for i in range(10):
-    print(i)
-    i = 5
-
+print(len(input("what is your name ? \n")))
