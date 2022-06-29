110266b0196441f178a3e9fcb67c5d755c3415e5
Abimbola2022 <olaideabimbola2021@gmail.com>
2022-06-29 08:44:35 -0400

    100-days-of-code

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+name_of_city = input("what's the name of city you grew up in ?\n")
+name_of_pet = input('what is the name of your pet ?\n')
+print("Your band name could be " + name_of_city + " " + name_of_pet + "\n")
---
+++ day2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator \n")
+total_bill = input("What was the total bill? $")
+split_bill = input("How many people to split the bill ")
+percentage_of_tip = input("What percentage tip would you like to give? 10, 12, or 15 ")
+
+print("Each person should pay: ")
